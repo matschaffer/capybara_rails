37e4d6db8f6104ad90d939f20bc8b33a48aac9d7
Mat Schaffer <mat@schaffer.me>
2012-02-24 07:54:54 -0500

    Properly skip railtie mixin when not in test mode

--- lib/capybara_rails/railtie.rb
+++ lib/capybara_rails/railtie.rb
@@ -1,12 +1,13 @@
 module CapybaraRails
   class Railtie < Rails::Railtie
     initializer "capybara_rails.initialize" do |app|
-      return unless Rails.env.test?
-      require 'capybara_rails/basic'
-      require 'capybara_rails/selenium'
+      if Rails.env.test?
+        require 'capybara_rails/basic'
+        require 'capybara_rails/selenium'
 
-      ActionDispatch::IntegrationTest.send(:include, CapybaraRails::Basic)
-      ActionDispatch::IntegrationTest.send(:include, CapybaraRails::Selenium)
+        ActionDispatch::IntegrationTest.send(:include, CapybaraRails::Basic)
+        ActionDispatch::IntegrationTest.send(:include, CapybaraRails::Selenium)
+      end
     end
   end
 end
